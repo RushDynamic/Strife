901dd4c145e80880a1423ff1f8feaee6f768fc20
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-07-01 02:28:40 +0530

    Changed header font

--- strife-client/src/components/Chat/Header/Header.jsx
+++ strife-client/src/components/Chat/Header/Header.jsx
@@ -9,7 +9,7 @@ export default function Header(props) {
     return (
         <>
             <div className={classes.headerContainer}>
-                <Typography variant="h2" style={{ letterSpacing: '2px', fontFamily: "'Syne', sans-serif" }}>strife</Typography>
+                <Typography variant="h2" style={{ paddingTop: '20px', letterSpacing: '1px', fontFamily: "'Syncopate', sans-serif" }}>strife</Typography>
                 {/* TODO: Add login/logout/control buttons under the header */}
                 <ChatMenu requestFriendsList={props.requestFriendsList} />
             </div>
