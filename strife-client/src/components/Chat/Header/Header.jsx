9b865112407dabbaa977b6cc2c2a0f678a4a3040
Rush Dynamic <tameimpalafanclubpresident@gmail.com>
2021-07-13 00:57:00 +0530

    Added constants file

--- strife-client/src/components/Chat/Header/Header.jsx
+++ strife-client/src/components/Chat/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import useStyles from '../../styles/chat-styles';
 import { Typography } from '@material-ui/core';
 import ChatMenu from './ChatMenu/ChatMenu.jsx';
+import constants from '../../../constants/strife-constants.js'
 
 export default function Header(props) {
     const classes = useStyles();
@@ -10,7 +11,7 @@ export default function Header(props) {
         <>
             <div className={classes.headerContainer}>
                 <Typography className={classes.nonSelectable} variant="h2" style={{ paddingTop: '20px', letterSpacing: '1px', fontFamily: "'Syncopate', sans-serif" }}>strife</Typography>
-                Version: 1.0.5 (PRE-ALPHA)
+                <Typography style={{ fontSize: '0.6rem' }}>{constants.APP_VERSION}</Typography>
                 {/* TODO: Add login/logout/control buttons under the header */}
                 <ChatMenu requestFriendsList={props.requestFriendsList} manageRooms={props.manageRooms} />
             </div>
