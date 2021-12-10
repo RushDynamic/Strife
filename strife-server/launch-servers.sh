ab0f582e2e9a864c261bb438a77a5e2d774cdc42
RushDynamic <tameimpalafanclubpresident@gmail.com>
2021-12-10 15:02:27 +0530

    Cleanup

--- strife-server/launch-servers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -z $MONGODB_CONNECTION_URI ]
-then
-    echo "Required environment variables have not been set, server not started"
-else
-    urxvt -e nodemon strife-chat-api/app &
-    urxvt -e nodemon account-management-api/app &
-    urxvt -e nodemon user-authorization-api/app &
-fi
