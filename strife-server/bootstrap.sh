0d294e9ea5013cae49b7a64a49cbc92833b1ba61
RushDynamic <tameimpalafanclubpresident@gmail.com>
2021-12-10 17:41:20 +0530

    Fixed env variable issue

--- strife-server/bootstrap.sh
+++ strife-server/bootstrap.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-if [ -z $MONGODB_CONNECTION_URI ]
-then
-    echo "Required environment variables have not been set, server not started"
-else
-    urxvt -e nodemon strife-chat-api/app &
-    urxvt -e nodemon account-management-api/app &
-    urxvt -e nodemon user-authorization-api/app &
-fi
+
+# cd required for recognizing .env file
+cd strife-chat-api && urxvt -e nodemon app &
+cd account-management-api && urxvt -e nodemon app &
+cd user-authorization-api && urxvt -e nodemon app &
