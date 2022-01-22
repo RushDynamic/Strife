1ae904e529f7029fe91dbb3b76a50b8b7a11faa7
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-01-22 16:58:01 +0530

    feat(avatar): added fallback default avatar

--- strife-client/src/components/Chat/Avatar.jsx
+++ strife-client/src/components/Chat/Avatar.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 
 function Avatar(props) {
+  const onlineStyle = { borderRadius: '50%', border: '2px solid green' };
+  const offlineStyle = { borderRadius: '50%' };
   return (
     <>
-      {props.online ? (
-        <img
-          alt="avatar_online"
-          src={props.avatarUrl}
-          style={{ borderRadius: '50%', border: '2px solid green' }}
-          height="25px"
-          width="25px"
-        />
-      ) : (
-        <img
-          alt="avatar_offline"
-          src={props.avatarUrl}
-          style={{ borderRadius: '50%' }}
-          height="25px"
-          width="25px"
-        />
-      )}
+      <img
+        alt="avatar_online"
+        src={props.avatarUrl}
+        style={props.online ? onlineStyle : offlineStyle}
+        height="25px"
+        width="25px"
+        // TODO: replace URL string with constant
+        onError={({ target }) => {
+          target.onerror = null;
+          target.src = 'http://localhost:3001/images/default_avatar.jpg';
+        }}
+      />
     </>
   );
 }
