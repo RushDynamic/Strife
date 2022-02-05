2cdfe4c27811a167b8b4e815c68630d1fba17114
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-02-05 22:08:45 +0530

    feat(client): hide logs in prod

--- strife-client/src/index.js
+++ strife-client/src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
+// replace console.* for disable log on production
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
