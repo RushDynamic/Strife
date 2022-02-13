ee2f91af3c7bb9b1677db1249e33868757edf552
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-02-13 18:15:27 +0530

    fix: broken avatar upload

--- strife-server/account-management-api/services/profile-service.js
+++ strife-server/account-management-api/services/profile-service.js
@@ -8,8 +8,12 @@ export async function uploadAvatar(file, username) {
   try {
     const extension = path.extname(file.name).toLowerCase();
     const newFileName = `${username}${new Date().getTime()}${extension}`;
-    const filePath = `${__dirname}/public/images/uploads/avatars/${newFileName}`;
-    await file.mv(filePath);
+    const filePath = `${__dirname}/public/images/uploads/avatars`;
+    // create avatars dir if it doesn't exist
+    if (!fs.existsSync(filePath)) {
+      fs.mkdirSync(filePath, { recursive: true });
+    }
+    await file.mv(`${filePath}/${newFileName}`);
     const fileStream = fs.createReadStream(filePath);
     const uploadParams = {
       Bucket: bucketName,
